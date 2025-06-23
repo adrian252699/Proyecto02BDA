c1855029f5e62f96bcd962882772e2232023221a
Carloss <luiscmg06@outlook.es>
2025-06-23 06:12:53 -0700

    Primeras pantallas

---
+++ Proyecto2_BDA/Proyecto2_BDA_Presentacion/src/main/java/Control/ControlNavegacion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Control;
+
+/**
+ *
+ * @author $Luis Carlos Manjarrez Gonzalez
+ */
+public class ControlNavegacion {
+    
+    public boolean validarCredenciales(String idTextField){
+        //implementar validacion tanto de ParseLong (contenga carracteres numericos)
+        // y verificar en BD
+        return true;
+    }
+
+}
